fb1c3bb98a0b36335de5563aca137edba35a72d7
Daffiq Trie Octorino <41116120010@student.mercubuana.ac.id>
2026-06-04 01:21:11 +0700

    Perubahan: Menambahkan yang belum ditambah

--- Produk/src/main/java/com/daffiqtrie/Produk/service/ProdukService.java
+++ Produk/src/main/java/com/daffiqtrie/Produk/service/ProdukService.java
@@ -7,24 +7,31 @@ import org.springframework.stereotype.Service;
 import com.daffiqtrie.Produk.repository.ProdukRepository;
 import com.daffiqtrie.Produk.model.Produk;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class ProdukService {
     @Autowired
     private ProdukRepository produkRepository;
 
     public List<Produk> getAllProduk() {
+        log.info("Fetching all products");
         return produkRepository.findAll();
     }
 
     public Produk getProdukById(Long id) {
+        log.info("Fetching product with id: {}", id);
         return produkRepository.findById(id).orElse(null);
     }
 
     public Produk createProduk(Produk produk) {
+        log.info("Creating product: {}", produk);
         return produkRepository.save(produk);
     }
 
     public void deleteProduk(Long id) {
+        log.info("Deleting product with id: {}", id);
         produkRepository.deleteById(id);
     }
 
